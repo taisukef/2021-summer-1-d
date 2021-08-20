2e4cc6c057e1e808da8523ee27291e9bbdb1f0ee
mochiyuuki <84883637+mochiyuuki@users.noreply.github.com>
2021-08-20 11:38:37 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
-import { Server } from "https://js.sabae.cc/Server.js";
+import { Server } from "./server/sabaeSever.js";
 
 const db = [];
 
 const commentGet = (req) => {
     let x = req.x;
     let y = req.y;
-    return { result: { x: x, y: y, data: db[x][y] } };
+    if (!x || !y){
+        return { result: {}, status: "error" }
+    }
+    if (db[x] == undefined || db[x][y] == undefined) {
+        return { result: {}, status: "error" }
+    }
+    return { result: { x: x, y: y, data: db[x][y] }, status: "success" };
 }
 
-const commentPost = (req) => {
+const commentPost = (req, cookies) => {
     let x = req.x;
     let y = req.y;
+    if (!x || !y){
+        return { status: "error" }
+    }
+    if(!req.data) {
+        return { status: "error" }
+    }
 
     if(!db[x]){
         db[x] = [];
@@ -19,20 +31,27 @@ const commentPost = (req) => {
         db[x][y] = [];
     }
     db[x][y].push(req.data);
-    return { result: "success" };
+
+    if ( !cookies.exp ) {
+        cookies.exp = 0;
+    } else {
+        cookies.exp++;
+    }
+    return { status: "success", cookie: { name: "exp", value: String(cookies.exp), path: "/" }};
 }
 
 class MyServer extends Server {
-    api(path, req) {
+    api(path, req, addr, cookies) {
+
         if (path.startsWith("/api/comment/get")) {
 
             let res = commentGet(req);
-            return { result: res.result };
+            return [{ result: res.result, status: res.status }, null];
 
         } else if (path.startsWith("/api/comment/post")) {
 
-            let res = commentPost(req);
-            return { status: res.result };
+            let res = commentPost(req, cookies);
+            return [{ status: res.status }, res.cookie];
 
         }
     }
